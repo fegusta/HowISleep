8c515fddca70febe41d5317d19b3aad03cf79283
felipe.nascimento <felipe.nascimento@certibio.com.br>
2019-12-18 18:01:04 -0300

    Inclusão de Activity de gravação

--- app/src/main/java/com/example/howisleep/MainActivity.kt
+++ app/src/main/java/com/example/howisleep/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.howisleep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_record_sleep.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text_start.setOnClickListener { view ->
+            val intent = Intent(this, RecordSleepActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/howisleep/RecordSleepActivity.kt
@@ -0,0 +1,220 @@
+package com.example.howisleep
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
+import android.transition.TransitionManager
+import android.util.Log
+import android.view.View
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_record_sleep.*
+import java.io.File
+import java.io.IOException
+
+class RecordSleepActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mRecorder: MediaRecorder? = null
+    private var mPlayer: MediaPlayer? = null
+    private var fileName: String? = null
+    private val RECORD_AUDIO_REQUEST_CODE = 101
+    private var lastProgress = 0
+    private var isPlaying = false
+    private var mHandler = Handler()
+
+    override fun onClick(view: View?) {
+        when (view!!.id) {
+            R.id.btn_record -> {
+                prepareRecording()
+                startRecording()
+            }
+
+            R.id.btn_stop -> {
+                prepareStop()
+                stopRecording()
+            }
+
+            R.id.btn_play -> {
+                if (!isPlaying && fileName != null) {
+                    isPlaying = true
+                    startPlaying()
+                } else {
+                    isPlaying = false
+                    stopPlaying()
+                }
+            }
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_sleep)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getPermissionToRecordAudio()
+        }
+
+        btn_record.setOnClickListener(this)
+        btn_stop.setOnClickListener(this)
+        btn_play.setOnClickListener(this)
+    }
+
+    private fun startRecording() {
+        mRecorder = MediaRecorder()
+        mRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        val root = android.os.Environment.getExternalStorageDirectory()
+        val file = File(root.absolutePath + "/AndroidCodility/Audios")
+        if (!file.exists()) {
+            file.mkdirs()
+        }
+
+        fileName = root.absolutePath + "/AndroidCodility/Audios/" + (System.currentTimeMillis().toString() + ".mp3")
+        Log.d("filename", fileName)
+        mRecorder!!.setOutputFile(fileName)
+        mRecorder!!.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+        try {
+            mRecorder!!.prepare()
+            mRecorder!!.start()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        lastProgress = 0
+        seekBar.progress = 0
+        stopPlaying()
+        // making the imageView a stop button starting the chronometer
+        chronometer.base = SystemClock.elapsedRealtime()
+        chronometer.start()
+    }
+
+    private fun stopRecording() {
+        try {
+            mRecorder!!.stop()
+            mRecorder!!.release()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        mRecorder = null
+        //starting the chronometer
+        chronometer.stop()
+        chronometer.base = SystemClock.elapsedRealtime()
+        //showing the play button
+        Toast.makeText(this, "Recording saved successfully.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startPlaying() {
+        mPlayer = MediaPlayer()
+        try {
+            mPlayer!!.setDataSource(fileName)
+            mPlayer!!.prepare()
+            mPlayer!!.start()
+        } catch (e: IOException) {
+            Log.e("LOG_TAG", "prepare() failed")
+        }
+
+        //making the imageView pause button
+        btn_play.setBackgroundResource(R.drawable.ic_pause_circle_white)
+
+        seekBar.progress = lastProgress
+        mPlayer!!.seekTo(lastProgress)
+        seekBar.max = mPlayer!!.duration
+        seekBarUpdate()
+        chronometer.start()
+
+        mPlayer!!.setOnCompletionListener(MediaPlayer.OnCompletionListener {
+            btn_play.setBackgroundResource(R.drawable.ic_play_white)
+            isPlaying = false
+            chronometer.stop()
+            chronometer.base = SystemClock.elapsedRealtime()
+            mPlayer!!.seekTo(0)
+        })
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                if (mPlayer != null && fromUser) {
+                    mPlayer!!.seekTo(progress)
+                    chronometer.base = SystemClock.elapsedRealtime() - mPlayer!!.currentPosition
+                    lastProgress = progress
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+    }
+
+    private var runnable: Runnable = Runnable { seekBarUpdate() }
+
+    private fun seekBarUpdate() {
+        if (mPlayer != null) {
+            val mCurrentPosition = mPlayer!!.currentPosition
+            seekBar.progress = mCurrentPosition
+            lastProgress = mCurrentPosition
+        }
+        mHandler.postDelayed(runnable, 100)
+    }
+
+    private fun stopPlaying() {
+        try {
+            mPlayer!!.release()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        mPlayer = null
+        //showing the play button
+        chronometer.stop()
+    }
+
+    private fun prepareRecording() {
+        btn_record.visibility = View.GONE
+        btn_stop.visibility = View.VISIBLE
+        btn_play.visibility = View.GONE
+        seekBar.visibility = View.GONE
+
+    }
+
+    private fun prepareStop() {
+        btn_record.visibility = View.VISIBLE
+        btn_stop.visibility = View.GONE
+        btn_play.visibility = View.VISIBLE
+        seekBar.visibility = View.VISIBLE
+    }
+
+    private fun getPermissionToRecordAudio() {
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid checking the build version since Context.checkSelfPermission(...) is only available in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted) since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE), RECORD_AUDIO_REQUEST_CODE)
+            }
+        }
+    }
+
+    // Callback with the request from calling requestPermissions(...)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == RECORD_AUDIO_REQUEST_CODE) {
+            if (grantResults.size == 3 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                //Toast.makeText(this, "Record Audio permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "You must give permissions to use this app. App is exiting.", Toast.LENGTH_SHORT).show()
+                finishAffinity()
+            }
+        }
+    }
+}
